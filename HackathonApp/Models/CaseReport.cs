c51aeafb14a793ea118a5a166b9e1f02485d48a0
Keshav Chaurasia <Keshav Chaurasia@KESHAVXPS>
2018-01-25 18:16:16 +0545

    Add project files.

---
+++ HackathonApp/Models/CaseReport.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HackathonApp.Models
+{
+    public class CaseReport
+    {
+        public int Id { get; set; }
+        public string PatientId { get; set; }
+        public MedicalIssue MedicalIssue { get; set; }
+        public string Weight { get; set; }
+        public string Height { get; set; }
+        public decimal BodyTemperature { get; set; }
+        public string PulseRate { get; set; }
+        public string RespirationRate { get; set; }
+        public string BloodPressure { get; set; }
+        public string SymptomDescription { get; set; }
+        public int? PatientImagesId { get; set; }
+        public int? OtherReportsId { get; set; }
+    }
+
+    public enum MedicalIssue {
+
+    }
+    public enum Sex {
+        Male=1,
+        Female =2,
+        Other = 3
+    }
+}
---
+++ HackathonApp/Models/Patient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HackathonApp.Models
+{
+    public class Patient
+    {
+        [Key]
+        public string UserId { get; set; }
+    }
+
+    
+}
